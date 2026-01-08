fe465848d5dedf3f2085e69f1afe1bac3bac466e
Noa <nguyenduynoa198@gmail.com>
2026-01-08 10:58:34 +0700

    feat: add apiPurchaseConfirm function and integrate order confirmation logic in the Order component with toast notifications

--- containers/purchase-order/order/hooks/useOrderList.js
+++ containers/purchase-order/order/hooks/useOrderList.js
@@ -1,6 +1,8 @@
 import apiOrder from "@/Api/apiPurchaseOrder/apiOrder";
 import { optionsQuery } from "@/configs/optionsQuery";
-import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { useLanguageContext } from "@/context/ui/LanguageContext";
+import useToast from "@/hooks/useToast";
+import { keepPreviousData, useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 export const useOrderList = (params) => {
     return useQuery({
@@ -24,5 +26,29 @@ export const useOrderListCode = () => {
             return rResult?.map((e) => ({ label: e.code, value: e.id }))
         },
         ...optionsQuery
+    })
+}
+
+export const useOrderConfirm = () => {
+    const showToast = useToast()
+    const queryClient = useQueryClient();
+    const dataLang = useLanguageContext(); 
+    return useMutation({
+        mutationFn: async ({ id, status }) => {
+            const data = { status };
+            const response = await apiOrder.apiPurchaseConfirm(id, data);
+            return response
+        },  
+        onSuccess: (data) => {
+            if(data.isSuccess){
+                showToast('success', dataLang[data.message] || 'Xác nhận đơn hàng thành công');
+                queryClient.invalidateQueries({ queryKey: ["api_list_order"] });
+            } else {
+                showToast('error', dataLang[data.message] || 'Xác nhận đơn hàng thất bại');
+            }
+        },
+        onError: (error) => {
+            console.log(error);
+        }
     })
 }
