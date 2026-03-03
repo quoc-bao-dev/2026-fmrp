257eac2290dee65091087dd83981c4f0129508d9
Quoc <pythagore1102@gmail.com>
2026-03-03 10:36:09 +0700

    fix: adjust CSS class in TabMaterialOutputHistory component for consistent layout

--- containers/manufacture/productions-orders/components/ui/TabMaterialOutputHistory.jsx
+++ containers/manufacture/productions-orders/components/ui/TabMaterialOutputHistory.jsx
@@ -54,7 +54,7 @@ const TabMaterialOutputHistory = ({ dataLang, items = [], count = 0 }) => {
                         </h4>
                     </div>
 
-                    <div className='col-span-16 grid grid-cols-16 min-h-[240px]'>
+                    <div className='col-span-16 grid grid-cols-16'>
                         {isLoadingTable ? (
                             <Loading className='3xl:h-full 2xl:h-full xl:h-full h-full col-span-16' />
                         ) : filteredData && filteredData.length > 0 ? (
